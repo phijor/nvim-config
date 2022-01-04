ed9aec52bc5c291ce6360bfd6503c52db2bd3321
Philipp Joram <mail@phijor.me>
2022-01-04 14:21:13 +0200

    feat(plugin): Use null-ls for more LSP sources

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -40,6 +40,23 @@ require('packer').startup(function (use)
       vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'config.lsp'.update_lightbulb()]]
     end
   }
+  use {
+    'jose-elias-alvarez/null-ls.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    },
+    config = function ()
+      local null_ls = require('null-ls')
+      null_ls.setup {
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.black,
+          null_ls.builtins.diagnostics.proselint,
+          null_ls.builtins.code_actions.gitsigns,
+        }
+      }
+    end
+  }
 
   -- Autocompletion
   use {
