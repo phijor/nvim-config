6575a90d8452a5ddfc9c39d0c8600b52a200ce50
Philipp Joram <mail@phijor.me>
2022-03-03 10:43:55 +0200

    feat: Use `sumneko-lua` builtin formatting

--- lua/phijor/plugins.lua
+++ lua/phijor/plugins.lua
@@ -56,7 +56,6 @@ require("packer").startup(function(use)
       local null_ls = require "null-ls"
       null_ls.setup {
         sources = {
-          null_ls.builtins.formatting.stylua,
           null_ls.builtins.formatting.black,
           null_ls.builtins.diagnostics.proselint,
           null_ls.builtins.code_actions.gitsigns.with {
