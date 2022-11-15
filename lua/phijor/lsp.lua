a8f454c5e53a6432de5a2c7d94f86ab055e0907d
Philipp Joram <mail@phijor.me>
2022-11-15 14:37:32 +0000

    refactor(lsp): Use `cmp_nvim_lsp` default capabilities

--- lua/phijor/lsp.lua
+++ lua/phijor/lsp.lua
@@ -27,8 +27,7 @@ end
 
 local lsp_get_default_config = function()
   -- Add additional capabilities supported by nvim-cmp
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+  local capabilities = require("cmp_nvim_lsp").default_capabilities()
   return {
     on_attach = default_on_attach,
     flags = {
