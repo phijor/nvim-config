c53c605dd86546d2efd288e29ff6cf8f84e0a5fc
Philipp Joram <mail@phijor.me>
2022-11-24 17:40:59 +0200

    refactor(plugin/nvim-tree): Configure via `setup()`
    Upstream has changed how the plugin is configured.

--- lua/phijor/plugins.lua
+++ lua/phijor/plugins.lua
@@ -296,13 +296,18 @@ require("packer").startup(function(use)
       },
     },
     config = function()
-      vim.g.nvim_tree_show_icons = {
-        git = 0,
-        folders = 0,
-        files = 0,
-        folder_arrows = 0,
+      require("nvim-tree").setup {
+        renderer = {
+          icons = {
+            show = {
+              file = false,
+              folder = false,
+              folder_arrow = false,
+              git = false,
+            }
+          }
+        }
       }
-      require("nvim-tree").setup {}
     end,
   }
 
